9ccf8c59d8da081028533b9a59fb5092a407cc36
Raj Quasar <rajjha.study@gmail.com>
2026-04-05 14:38:05 +0530

    mailer update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,7 +2,7 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 const transporter = nodemailer.createTransport({
-  host: "gmail",
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
