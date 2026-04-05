b97c9b4dad2693896c7c7d255432b4ec46958ce2
Raj Quasar <rajjha.study@gmail.com>
2026-04-05 14:16:27 +0530

    mailer update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,31 +1,26 @@
-// @ts-ignore
-import SibApiV3Sdk from "sib-api-v3-sdk";
-import fs from "fs";
+// utils/mailer.js
+import nodemailer from "nodemailer";
 import "dotenv/config";
-
-const client = SibApiV3Sdk.ApiClient.instance;
-const apiKey = client.authentications["api-key"];
-apiKey.apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  connectionTimeout: 10000, // 10 seconds
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.SECRET_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // Prevents SSL issues on cloud servers
+  },
+});
 
 const sendMail = async ({ email, company, filePath, name }) => {
-  if (!filePath) {
-    throw new Error("Resume required ❌");
-  }
-
-  const sendSmtpEmail = {
-    sender: {
-      email: process.env.EMAIL_USER,
-      name: "Raj Kumar Jha",
-    },
-    to: [
-      {
-        email: email,
-      },
-    ],
-    subject: "Application for Frontend Developer Role",
-    textContent: `Hello ${name || "Hiring Team"},
+  return transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: `Application for Frontend Developer Role`,
+    text: `Hello ${name || "Hiring Team"},
 
 I hope you're doing well. I’m excited to apply for a Frontend Developer (Fresher) opportunity at ${company || "your company"}. I have hands on experience in React.js, JavaScript, HTML, and CSS, building responsive and user-friendly web applications.
 
@@ -50,15 +45,17 @@ Thank you for your time and consideration.
 Warm regards,  
 Raj Kumar Jha  
 📞 ${process.env.PHONE_USER} | ✉️ ${process.env.EMAIL_USER}`,
-    attachment: [
-      {
-        name: "Raj_resume.pdf",
-        content: fs.readFileSync(filePath).toString("base64"),
-      },
-    ],
-  };
-
-  return await apiInstance.sendTransacEmail(sendSmtpEmail);
+    attachments: filePath
+      ? [
+          {
+            filename: "Raj_resume.pdf",
+            path: filePath,
+          },
+        ]
+      : (() => {
+          throw new Error("Resume is required ❌");
+        })(),
+  });
 };
 
 export default sendMail;
